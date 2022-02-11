8e8de0593cbdb6d29472117983de89859914e3c4
awatersny <awatersny@gmail.com>
2022-02-11 11:50:55 -0500

    Set up initial variables.

--- js/app.js
+++ js/app.js
@@ -1,25 +1,52 @@
 /*----------------------------------------Constants------------------------------------------*/
 // Array containing a sequence for the player to memorize:
 // This array will have a random number from 0-3 pushed into it at the beginning of each turn.
+const compSequence = [];
 
 // Array containing a sequence corresponding to player input:
 // This array will contain a value from 0-3 corresponding with the element clicked by the player.
-
+const playerSequence = [];
 
 /*----------------------------------------Variables------------------------------------------*/
 // Score: Increments each time the two arrays are congruent.
+let score = 0;
 // High Score: Increments when score is greater than or equal to it.
+let hiScore = 0;
+// Turn: 0 for computer and 1 for player
+let turn = null;
 
 /*--------------------------------Cached Element References----------------------------------*/
-// 4 clickable elements of different colors
-// A start button
-// A reset button
-// Rendered score
+// Status message
+const statusMsg = document.getElementById("status");
 // Rendered high score
+const hiScoreDisplay = document.getElementById("hi-score");
+// 4 clickable elements of different colors
+const board = document.getElementById("board");
+const colorBtns = document.querySelectorAll("board-button");
+// A start button
+const startBtn = document.getElementById("start");
+// A reset button
+const resetBtn = document.getElementById("reset");
+// Rendered score
+const scoreDisplay = document.getElementById("score");
+
+/*------------------------------------Event Listeners----------------------------------------*/
+board.addEventListener("click", testEvent); //Note: if evt.target.id === board, nothing should happen.
+startBtn.addEventListener("click", testEvent);
+resetBtn.addEventListener("click", testEvent);
 
 /*---------------------------------------Functions-------------------------------------------*/
 // Initial state
+function init(){
+
+}
+
 
 // Sequences should start when the player presses a start button.
 // Four elements light up (change to a lighter hue of their color: start with 0.5 seconds) in random order, but the sequences should be stored.
 // The player should be able to reset the game at any time.
+
+
+function testEvent(evt){
+  console.log(evt.target.id);
+}
