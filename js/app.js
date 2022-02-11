f69e95cc1d9adf1f6c80b23dc76821f80b562790
awatersny <awatersny@gmail.com>
2022-02-11 12:23:33 -0500

    Test Event Listeners

--- js/app.js
+++ js/app.js
@@ -12,8 +12,8 @@ const playerSequence = [];
 let score = 0;
 // High Score: Increments when score is greater than or equal to it.
 let hiScore = 0;
-// Turn: 0 for computer and 1 for player
-let turn = null;
+// Turn: -1 for computer and 1 for player. 0 if game is not in session
+let turn = 0;
 
 /*--------------------------------Cached Element References----------------------------------*/
 // Status message
@@ -36,17 +36,29 @@ startBtn.addEventListener("click", testEvent);
 resetBtn.addEventListener("click", testEvent);
 
 /*---------------------------------------Functions-------------------------------------------*/
-// Initial state
-function init(){
-
-}
-
-
 // Sequences should start when the player presses a start button.
 // Four elements light up (change to a lighter hue of their color: start with 0.5 seconds) in random order, but the sequences should be stored.
 // The player should be able to reset the game at any time.
 
+// Initial state
+function init(){
+  score = 0;
+  turn = 0;
+  render();
+}
 
+function render() {
+  if(turn) {
+    startBtn.setAttribute("hidden", true);
+    resetBtn.removeAttribute("hidden");
+  } else {
+    resetBtn.setAttribute("hidden", true);
+    startBtn.removeAttribute("hidden");
+  }
+}
+init();
+
+// Temporary test function to be deleted.
 function testEvent(evt){
   console.log(evt.target.id);
 }
