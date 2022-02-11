fa2a9cef87e4bcfc03f9d2e644e736088bdfc76e
awatersny <awatersny@gmail.com>
2022-02-11 10:52:50 -0500

    Lay out JS structure

---
+++ js/app.js
@@ -0,0 +1,25 @@
+/*----------------------------------------Constants------------------------------------------*/
+// Array containing a sequence for the player to memorize:
+// This array will have a random number from 0-3 pushed into it at the beginning of each turn.
+
+// Array containing a sequence corresponding to player input:
+// This array will contain a value from 0-3 corresponding with the element clicked by the player.
+
+
+/*----------------------------------------Variables------------------------------------------*/
+// Score: Increments each time the two arrays are congruent.
+// High Score: Increments when score is greater than or equal to it.
+
+/*--------------------------------Cached Element References----------------------------------*/
+// 4 clickable elements of different colors
+// A start button
+// A reset button
+// Rendered score
+// Rendered high score
+
+/*---------------------------------------Functions-------------------------------------------*/
+// Initial state
+
+// Sequences should start when the player presses a start button.
+// Four elements light up (change to a lighter hue of their color: start with 0.5 seconds) in random order, but the sequences should be stored.
+// The player should be able to reset the game at any time.
